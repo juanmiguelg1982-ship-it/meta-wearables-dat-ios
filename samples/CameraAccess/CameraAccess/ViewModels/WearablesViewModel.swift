21512bb68bb9a5fe638f5489e8a02833aa94b8bb
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-10 14:28:08 +0200

    Update WearablesViewModel.swift

--- samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
@@ -31,8 +31,10 @@ class WearablesViewModel: ObservableObject {
       }
     }
     Task {
-      guard let url = URL(string: "https://bidjuanmi.com/chat-stream?message=AppArranc%C3%B3") else { return }
-      _ = try? await URLSession.shared.data(from: url)
+      guard let url = URL(string: "http://79.116.235.125:8000/chat-stream?message=AppArranc%C3%B3") else { return }
+      var request = URLRequest(url: url)
+      request.timeoutInterval = 10
+      _ = try? await URLSession.shared.data(for: request)
     }
   }
 
