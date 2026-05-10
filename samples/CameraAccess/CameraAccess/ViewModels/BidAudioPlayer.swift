1c24835c33fd3a8ecda0cd4291162ab62132f9c9
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-11 00:44:49 +0200

    Update BidAudioPlayer.swift

--- samples/CameraAccess/CameraAccess/ViewModels/BidAudioPlayer.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/BidAudioPlayer.swift
@@ -1,29 +1,18 @@
 final class BidAudioPlayer: NSObject, @unchecked Sendable {
   static let shared = BidAudioPlayer()
-  private var player: AVPlayer?
+  private var player: AVAudioPlayer?
 
   private override init() {
     super.init()
   }
 
   func play(data: Data) {
-  let url = FileManager.default.temporaryDirectory.appendingPathComponent("bid_response.mp3")
-  try? data.write(to: url)
-  
-  // Forzar salida por Bluetooth
-  try? AVAudioSession.sharedInstance().setCategory(
-    .playback,
-    mode: .default,
-    options: [.allowBluetoothHFP]
-  )
-  try? AVAudioSession.sharedInstance().setActive(true)
-  
-  // Esperar a que Bluetooth esté listo
-  DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-    let item = AVPlayerItem(url: url)
-    self.player = AVPlayer(playerItem: item)
-    self.player?.volume = 1.0
-    self.player?.play()
+    do {
+      player = try AVAudioPlayer(data: data)
+      player?.prepareToPlay()
+      player?.play()
+    } catch {
+      print("BidAudioPlayer error: \(error)")
+    }
   }
-}
 }
