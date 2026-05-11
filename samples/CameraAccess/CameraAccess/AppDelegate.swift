506e24cfbb8ff0ea72045ee5ea9ec5588207cffd
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-12 01:23:53 +0200

    Update AppDelegate.swift

--- samples/CameraAccess/CameraAccess/AppDelegate.swift
+++ samples/CameraAccess/CameraAccess/AppDelegate.swift
@@ -10,7 +10,22 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   }
 
   func applicationDidEnterBackground(_ application: UIApplication) {
-    // Asegurar que el audio sigue activo en segundo plano
+    try? AVAudioSession.sharedInstance().setCategory(
+      .playAndRecord,
+      mode: .voiceChat,
+      options: [.allowBluetoothHFP, .mixWithOthers]
+    )
+    try? AVAudioSession.sharedInstance().setActive(true)
+    silencioPlayer?.play()
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+    try? AVAudioSession.sharedInstance().setCategory(
+      .playAndRecord,
+      mode: .voiceChat,
+      options: [.allowBluetoothHFP, .mixWithOthers]
+    )
+    try? AVAudioSession.sharedInstance().setActive(true)
     silencioPlayer?.play()
   }
 
@@ -21,8 +36,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   private func configurarAudioBackground() {
     try? AVAudioSession.sharedInstance().setCategory(
       .playAndRecord,
-      mode: .default,
-      options: [.allowBluetooth, .mixWithOthers]
+      mode: .voiceChat,
+      options: [.allowBluetoothHFP, .mixWithOthers]
     )
     try? AVAudioSession.sharedInstance().setActive(true)
 
@@ -30,12 +45,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     let numSamples = Int(sampleRate * 0.5)
     var silencio = [Float](repeating: 0.0, count: numSamples)
     let data = Data(bytes: &silencio, count: numSamples * 4)
-
     guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("silencio.wav") else { return }
 
     var wav = Data()
     let dataSize = UInt32(data.count)
-    let sampleRateInt = UInt32(sampleRate)
+    let sr = UInt32(sampleRate)
     wav.append("RIFF".data(using: .ascii)!)
     wav.append(withUnsafeBytes(of: (36 + dataSize).littleEndian) { Data($0) })
     wav.append("WAVE".data(using: .ascii)!)
@@ -43,14 +57,13 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     wav.append(withUnsafeBytes(of: UInt32(16).littleEndian) { Data($0) })
     wav.append(withUnsafeBytes(of: UInt16(3).littleEndian) { Data($0) })
     wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
-    wav.append(withUnsafeBytes(of: sampleRateInt.littleEndian) { Data($0) })
-    wav.append(withUnsafeBytes(of: (sampleRateInt * 4).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: sr.littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: (sr * 4).littleEndian) { Data($0) })
     wav.append(withUnsafeBytes(of: UInt16(4).littleEndian) { Data($0) })
     wav.append(withUnsafeBytes(of: UInt16(32).littleEndian) { Data($0) })
     wav.append("data".data(using: .ascii)!)
     wav.append(withUnsafeBytes(of: dataSize.littleEndian) { Data($0) })
     wav.append(data)
-
     try? wav.write(to: url)
 
     if let player = try? AVAudioPlayer(contentsOf: url) {
