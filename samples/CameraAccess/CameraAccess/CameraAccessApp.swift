6fd607b66a8f3324734c8e525ac5a20209556fdd
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-11 22:59:24 +0200

    Update CameraAccessApp.swift

--- samples/CameraAccess/CameraAccess/CameraAccessApp.swift
+++ samples/CameraAccess/CameraAccess/CameraAccessApp.swift
@@ -15,6 +15,7 @@ struct CameraAccessApp: App {
   #endif
   private let wearables: WearablesInterface
   @StateObject private var wearablesViewModel: WearablesViewModel
+  private var silencioPlayer: AVAudioPlayer?
 
   init() {
     do {
@@ -27,18 +28,53 @@ struct CameraAccessApp: App {
     let wearables = Wearables.shared
     self.wearables = wearables
     self._wearablesViewModel = StateObject(wrappedValue: WearablesViewModel(wearables: wearables))
-
-    // Mantener app activa en segundo plano para el audio
     setupBackgroundAudio()
   }
 
-  private func setupBackgroundAudio() {
+  private mutating func setupBackgroundAudio() {
     try? AVAudioSession.sharedInstance().setCategory(
       .playAndRecord,
       mode: .default,
       options: [.allowBluetooth, .mixWithOthers]
     )
     try? AVAudioSession.sharedInstance().setActive(true)
+    reproducirSilencio()
+  }
+
+  private mutating func reproducirSilencio() {
+    let sampleRate = 44100.0
+    let numSamples = Int(sampleRate * 0.1)
+    var silencio = [Float](repeating: 0.0, count: numSamples)
+    let data = Data(bytes: &silencio, count: numSamples * 4)
+
+    guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("silencio.wav") else { return }
+
+    var wav = Data()
+    let dataSize = UInt32(data.count)
+    let sampleRateInt = UInt32(sampleRate)
+    wav.append("RIFF".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: (36 + dataSize).littleEndian) { Data($0) })
+    wav.append("WAVE".data(using: .ascii)!)
+    wav.append("fmt ".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: UInt32(16).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(3).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: sampleRateInt.littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: (sampleRateInt * 4).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(4).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(32).littleEndian) { Data($0) })
+    wav.append("data".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: dataSize.littleEndian) { Data($0) })
+    wav.append(data)
+
+    try? wav.write(to: url)
+
+    if let player = try? AVAudioPlayer(contentsOf: url) {
+      player.numberOfLoops = -1
+      player.volume = 0.0
+      player.play()
+      silencioPlayer = player
+    }
   }
 
   var body: some Scene {
