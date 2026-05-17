1baaa7f3faf38d030683848a4bc58daa43d37e11
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-17 16:16:13 +0200

    Update BidAudioPlayer.swift

--- samples/CameraAccess/CameraAccess/ViewModels/BidAudioPlayer.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/BidAudioPlayer.swift
@@ -1,18 +1,25 @@
-final class BidAudioPlayer: NSObject, @unchecked Sendable {
-  static let shared = BidAudioPlayer()
-  private var player: AVAudioPlayer?
-
-  private override init() {
-    super.init()
-  }
-
-  func play(data: Data) {
-    do {
-      player = try AVAudioPlayer(data: data)
-      player?.prepareToPlay()
-      player?.play()
-    } catch {
-      print("BidAudioPlayer error: \(error)")
+final class BidAudioPlayer: NSObject, AVAudioPlayerDelegate, @unchecked Sendable {
+    static let shared = BidAudioPlayer()
+    private var player: AVAudioPlayer?
+    private override init() {
+        super.init()
     }
-  }
+    func play(data: Data) {
+        do {
+            player = try AVAudioPlayer(data: data)
+            player?.delegate = self
+            player?.prepareToPlay()
+            player?.play()
+        } catch {
+            print("BidAudioPlayer error: \(error)")
+        }
+    }
+    func stop() {
+        player?.stop()
+        player = nil
+        NotificationCenter.default.post(name: NSNotification.Name("BIDAudioTerminado"), object: nil)
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        NotificationCenter.default.post(name: NSNotification.Name("BIDAudioTerminado"), object: nil)
+    }
 }
