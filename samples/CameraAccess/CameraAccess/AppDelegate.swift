baffea7bf10226d3c037c0947128d2790976d163
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-05-18 18:27:57 +0200

    Update AppDelegate.swift

--- samples/CameraAccess/CameraAccess/AppDelegate.swift
+++ samples/CameraAccess/CameraAccess/AppDelegate.swift
@@ -1,52 +1,44 @@
 import AVFoundation
 import UIKit
-
 class AppDelegate: NSObject, UIApplicationDelegate {
   var silencioPlayer: AVAudioPlayer?
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     configurarAudioBackground()
     return true
   }
-
   func applicationDidEnterBackground(_ application: UIApplication) {
     try? AVAudioSession.sharedInstance().setCategory(
       .playAndRecord,
-      mode: .voiceChat,
-      options: [.allowBluetoothHFP, .mixWithOthers]
+      mode: .default,
+      options: [.allowBluetoothHFP, .mixWithOthers, .defaultToSpeaker]
     )
     try? AVAudioSession.sharedInstance().setActive(true)
     silencioPlayer?.play()
   }
-
   func applicationWillResignActive(_ application: UIApplication) {
     try? AVAudioSession.sharedInstance().setCategory(
       .playAndRecord,
-      mode: .voiceChat,
-      options: [.allowBluetoothHFP, .mixWithOthers]
+      mode: .default,
+      options: [.allowBluetoothHFP, .mixWithOthers, .defaultToSpeaker]
     )
     try? AVAudioSession.sharedInstance().setActive(true)
     silencioPlayer?.play()
   }
-
   func applicationWillEnterForeground(_ application: UIApplication) {
     silencioPlayer?.play()
   }
-
   private func configurarAudioBackground() {
     try? AVAudioSession.sharedInstance().setCategory(
       .playAndRecord,
-      mode: .voiceChat,
-      options: [.allowBluetoothHFP, .mixWithOthers]
+      mode: .default,
+      options: [.allowBluetoothHFP, .mixWithOthers, .defaultToSpeaker]
     )
     try? AVAudioSession.sharedInstance().setActive(true)
-
     let sampleRate = 44100.0
     let numSamples = Int(sampleRate * 0.5)
     var silencio = [Float](repeating: 0.0, count: numSamples)
     let data = Data(bytes: &silencio, count: numSamples * 4)
     guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("silencio.wav") else { return }
-
     var wav = Data()
     let dataSize = UInt32(data.count)
     let sr = UInt32(sampleRate)
@@ -65,7 +57,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     wav.append(withUnsafeBytes(of: dataSize.littleEndian) { Data($0) })
     wav.append(data)
     try? wav.write(to: url)
-
     if let player = try? AVAudioPlayer(contentsOf: url) {
       player.numberOfLoops = -1
       player.volume = 0.0
