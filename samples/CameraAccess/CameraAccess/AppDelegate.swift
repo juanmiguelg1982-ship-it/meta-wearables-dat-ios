007dc226461a60d07859e3747ef3c611f6df6f53
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-06-03 20:08:10 +0200

    Update AppDelegate.swift

--- samples/CameraAccess/CameraAccess/AppDelegate.swift
+++ samples/CameraAccess/CameraAccess/AppDelegate.swift
@@ -44,33 +44,40 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
 
     func iniciarSilencioLoop() {
-        let sampleRate: UInt32 = 44100
-        let numSamples = Int(sampleRate)
-        let samples = [Int16](repeating: 0, count: numSamples)
-        var wav = Data()
-        let dataSize = UInt32(numSamples * 2)
-        wav.append("RIFF".data(using: .ascii)!)
-        wav.append(withUnsafeBytes(of: (36 + dataSize).littleEndian) { Data($0) })
-        wav.append("WAVE".data(using: .ascii)!)
-        wav.append("fmt ".data(using: .ascii)!)
-        wav.append(withUnsafeBytes(of: UInt32(16).littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: sampleRate.littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: (sampleRate * 2).littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: UInt16(2).littleEndian) { Data($0) })
-        wav.append(withUnsafeBytes(of: UInt16(16).littleEndian) { Data($0) })
-        wav.append("data".data(using: .ascii)!)
-        wav.append(withUnsafeBytes(of: dataSize.littleEndian) { Data($0) })
-        wav.append(Data(bytes: samples, count: numSamples * 2))
-        do {
-            silencioPlayer = try AVAudioPlayer(data: wav)
-            silencioPlayer?.numberOfLoops = -1
-            silencioPlayer?.volume = 0.0
-            silencioPlayer?.prepareToPlay()
-            silencioPlayer?.play()
-        } catch {}
+    let sampleRate: UInt32 = 44100
+    let numSamples = Int(sampleRate)
+    let samples = [Int16](repeating: 0, count: numSamples)
+    var wav = Data()
+    let dataSize = UInt32(numSamples * 2)
+    wav.append("RIFF".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: (36 + dataSize).littleEndian) { Data($0) })
+    wav.append("WAVE".data(using: .ascii)!)
+    wav.append("fmt ".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: UInt32(16).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(1).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: sampleRate.littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: (sampleRate * 2).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(2).littleEndian) { Data($0) })
+    wav.append(withUnsafeBytes(of: UInt16(16).littleEndian) { Data($0) })
+    wav.append("data".data(using: .ascii)!)
+    wav.append(withUnsafeBytes(of: dataSize.littleEndian) { Data($0) })
+    wav.append(Data(bytes: samples, count: numSamples * 2))
+    do {
+        silencioPlayer = try AVAudioPlayer(data: wav)
+        silencioPlayer?.numberOfLoops = -1
+        silencioPlayer?.volume = 0.0
+        silencioPlayer?.prepareToPlay()
+        silencioPlayer?.play()
+    } catch {}
+
+    Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { [weak self] _ in
+        if self?.silencioPlayer?.isPlaying == false {
+            self?.silencioPlayer?.play()
+        }
+        try? AVAudioSession.sharedInstance().setActive(true)
     }
+}
 
     private func renovarBackgroundTask() {
         terminarBackgroundTask()
