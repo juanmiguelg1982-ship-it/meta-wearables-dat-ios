8f210fe31f6ea3f6a081ddb7c171f90c74f7a08a
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-06-08 23:11:08 +0200

    Update WearablesViewModel.swift

--- samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
@@ -111,12 +111,14 @@ final class BidEscuchaManager: NSObject, SFSpeechRecognizerDelegate {
         onEstado("⏸ Bid desactivado (Tesla)")
     }
 
-   func activarTodo() {
+  func activarTodo() {
     URLSession.shared.dataTask(with: URL(string: "https://bidjuanmi.com/bid-log?msg=activarTodo-llamado")!).resume()
     pausadoPorSistema = false
-    try? AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .voiceChat, options: [.allowBluetoothHFP, .mixWithOthers])
-    try? AVAudioSession.sharedInstance().setActive(true)
-    iniciarEscuchaBID()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        try? AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .voiceChat, options: [.allowBluetoothHFP, .mixWithOthers])
+        try? AVAudioSession.sharedInstance().setActive(true)
+        self.iniciarEscuchaBID()
+    }
 }
 
     func reanudarPorSistema() {
