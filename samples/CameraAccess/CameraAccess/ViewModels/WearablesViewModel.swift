ddbb8b5584564066345d424244bb26386983484f
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-06-10 20:01:26 +0200

    Update WearablesViewModel.swift

--- samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
@@ -327,27 +327,16 @@ func reanudarPorTesla() {
     Task { await onPregunta(transcripcion) }
   }
 
-  private func pararEngine(resetearEngine: Bool = false) {
+ private func pararEngine() {
     vigilanteTask?.cancel()
     envioTimer?.invalidate()
     recognitionTask?.cancel()
     recognitionTask = nil
     recognitionRequest?.endAudio()
     recognitionRequest = nil
-    if audioEngine.isRunning {
-        audioEngine.inputNode.removeTap(onBus: 0)
-        audioEngine.stop()
-    } else {
-        try? audioEngine.inputNode.removeTap(onBus: 0)
-    }
-    if resetearEngine {
-        audioEngine = AVAudioEngine()
-    }
+    if audioEngine.isRunning { audioEngine.inputNode.removeTap(onBus: 0) }
 }
 
-func resetearEngineExterno() {
-    pararEngine(resetearEngine: true)
-}
 
 private func arrancarEngine() {
     let inputNode = audioEngine.inputNode
