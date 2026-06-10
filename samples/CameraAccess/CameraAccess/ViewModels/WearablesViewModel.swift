dd91c1b381612dc3d1f4d114a5ec6177660c8e54
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-06-10 21:12:44 +0200

    Update WearablesViewModel.swift

--- samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
@@ -122,7 +122,7 @@ func reanudarPorTesla() {
     if tipo == .began {
         let msg = "INTERRUPCION-began"
         URLSession.shared.dataTask(with: URL(string: "https://bidjuanmi.com/bid-log?msg=\(msg)")!).resume()
-        pararEngine()
+        pararEngine(detener: true)
         try? AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
     } else if tipo == .ended {
         let msg = "INTERRUPCION-ended"
@@ -337,15 +337,20 @@ func reanudarPorTesla() {
     Task { await onPregunta(transcripcion) }
   }
 
-  private func pararEngine() {
+  private func pararEngine(detener: Bool = false) {
     vigilanteTask?.cancel()
     envioTimer?.invalidate()
     recognitionTask?.cancel()
     recognitionTask = nil
     recognitionRequest?.endAudio()
     recognitionRequest = nil
-    if audioEngine.isRunning { audioEngine.inputNode.removeTap(onBus: 0) }
-  }
+    if audioEngine.isRunning {
+        audioEngine.inputNode.removeTap(onBus: 0)
+        if detener { audioEngine.stop() }
+    } else {
+        audioEngine.inputNode.removeTap(onBus: 0)
+    }
+}
 
  private func arrancarEngine() {
     let inputNode = audioEngine.inputNode
