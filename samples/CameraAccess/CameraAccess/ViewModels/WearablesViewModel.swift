5f4504c63663ce3c64ef16b0aa17615fb95458d8
juanmiguelg1982-ship-it <juanmiguelg1982@gmail.com>
2026-06-11 20:14:47 +0200

    Update WearablesViewModel.swift

--- samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
+++ samples/CameraAccess/CameraAccess/ViewModels/WearablesViewModel.swift
@@ -220,7 +220,7 @@ URLSession.shared.dataTask(with: URL(string: "https://bidjuanmi.com/bid-log?msg=
       if error != nil {
     guard !self.faseEscucha, !self.enConversacion else { return }
     DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
-        guard !self.enConversacion, !self.grabandoRespuesta, !self.pausadoPorSistema else { return }
+        guard !self.enConversacion, !self.grabandoRespuesta, !self.pausadoPorSistema, !self.audioEngine.isRunning else { return }
         self.iniciarEscuchaBID()
     }
 }
